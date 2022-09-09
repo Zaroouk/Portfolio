a5faa388a95db857f39b50323b90e855e12ef6bb
Emanuel Pena <emanuel@Emanuels-MacBook-Pro.local>
2022-09-08 20:49:33 -0600

    first commit

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Navbar, Button, Link, Text } from '@nextui-org/react'
+
+function HomePage() {
+  return (
+
+
+      <Navbar isBordered variant="floating">
+        <Navbar.Brand>
+          <AcmeLogo />
+          <Text b color="inherit" hideIn="xs">
+            ACME
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content hideIn="xs" variant="highlight-rounded">
+          <Navbar.Link href="#">Features</Navbar.Link>
+          <Navbar.Link isActive href="#">Customers</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Company</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link color="inherit" href="#">
+            Login
+          </Navbar.Link>
+          <Navbar.Item>
+            <Button auto flat as={Link} href="#">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+  )
+}
+
+export default HomePage
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { NextUIProvider } from '@nextui-org/react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+    <NextUIProvider>
+    <App />
+    </NextUIProvider>
+
+)
